f48ba0572b02d3a677f34757418c3f4d15850901
AleksanderSkubala <alexskubala@gmail.com>
2024-02-09 20:10:19 +0100

    changed model version and changed chunks printing

--- utils/queries.py
+++ utils/queries.py
@@ -4,6 +4,9 @@ from langchain_openai import ChatOpenAI
 from langchain.retrievers import ContextualCompressionRetriever
 from langchain.retrievers.document_compressors import LLMChainExtractor
 
+def create_prompt_template():
+  return "abd"
+
 def create_qa_chain(model_name):
   llm = ChatOpenAI(model_name=model_name, temperature=0)
   chain = load_qa_chain(
@@ -27,16 +30,21 @@ def create_compression_retriever(vectordb, model_name):
 def get_precise_response(query: str, vectordb, chain):
   matching_chunks = vectordb.similarity_search(query,k=3)
   print("Chunks of text matching the question:")
-  print(f"{matching_chunks}")
+
+  print("Chunks of text matching the question:")
+  for chunk in matching_chunks:
+    print(f"Page number: {chunk.metadata["page"]}")
 
   answer = chain.run(input_documents=matching_chunks, question=query)
   return answer
 
 def get_diversed_answer(query, vectordb, chain):
   # based on Maximum Marginal Relevance (MMR)
-  matching_chunks = vectordb.max_marginal_relevance_search(query,k=3, fetch_k=3)
+  matching_chunks = vectordb.max_marginal_relevance_search(query,k=2, fetch_k=2)
+
   print("Chunks of text matching the question:")
-  print(f"{matching_chunks}")
+  for chunk in matching_chunks:
+    print(f"Page number: {chunk.metadata["page"]}")
 
   answer = chain.run(input_documents=matching_chunks, question=query)
   return answer
@@ -45,7 +53,11 @@ def get_compressed_context_answer(query, compression_retriever, chain):
   # based on Context Compression
   matching_chunks = compression_retriever.get_relevant_documents(query)
   print("Chunks of text matching the question:")
-  print(f"{matching_chunks}")
+
+  print("Chunks of text matching the question:")
+  for chunk in matching_chunks:
+    print(f"Page number: {chunk.metadata["page"]}")
+
 
   answer = chain.run(input_documents=matching_chunks, question=query)
   return answer
