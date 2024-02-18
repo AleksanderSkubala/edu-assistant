83bc6c9134baaf19e30f8e72c2faab811770ee5f
AleksanderSkubala <alexskubala@gmail.com>
2024-02-18 23:23:02 +0100

    started hinting/typing refactor

--- utils/summarize.py
+++ utils/summarize.py
@@ -6,22 +6,27 @@ from langchain.prompts import PromptTemplate
 from langchain.chains import load_summarize_chain
 from langchain.chains.combine_documents.stuff import StuffDocumentsChain
 
+from langchain.docstore.document import Document
 from langchain.chains import create_extraction_chain
+
+from typing import Literal
+
+type SummaryChainType = Literal["stuff", "map_reduce", "refine"]
 
 llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo-1106")
 
-def extract_data(input_data, schema):
+def extract_data(input_data: str, schema: object) -> str:
   extraction_llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
   extraction_chain = create_extraction_chain(schema, extraction_llm)
   return extraction_chain.run(input_data)
 
-def summarize_docs(docs, type, schema):
+def summarize_docs(docs: list[Document], type: SummaryChainType, schema: object) -> str:
   chain = load_summarize_chain(llm, chain_type=type)
   summary = chain.run(docs)
 
   return extract_data(summary, schema)
 
-def get_general_topics(docs):
+def get_general_topics(docs: list[Document]) -> str:
   schema = {
     "properties": {
       "general_topics": {
@@ -34,10 +39,10 @@ def get_general_topics(docs):
 
   return summarize_docs(docs, "stuff", schema)
 
-def get_detailed_topics(docs):
+def get_detailed_topics(docs: list[Document]) -> str:
   schema = {
     "properties": {
-      "topics": {
+      "detailed_topics": {
         "type": "array",
         "items": {"type": "string"}
       },
@@ -47,7 +52,7 @@ def get_detailed_topics(docs):
 
   return summarize_docs(docs, "map_reduce", schema)
 
-def get_combined_topics(docs):
+def get_combined_topics(docs: list[Document]) -> str:
   schema = {
     "properties": {
       "general_topics": {
